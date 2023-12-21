cac0e2c1d9e05519de2b0c5823d9cbe1ec49e966
Krisna Rhesa <pradnyana.2205551073@student.unud.ac.id>
2023-12-21 15:27:08 +0800

    env

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+
+import {createApp} from 'vue';
+
+import App from '..src/App.vue';
+
+
+const app = createApp(App);
+
+app.mount('#app');
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/{any}', function () {
+    return view('app');
+}) ->where('any', '.*');
