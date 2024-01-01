bfa8e71fc5c2c0c178232376e89ebaae6be10834
RevaPrasetya <revabro3@gmail.com>
2024-01-02 00:06:54 +0800

    link html

--- frontend-vue/src/components/Sidebar.vue
+++ frontend-vue/src/components/Sidebar.vue
@@ -558,8 +558,7 @@ const logout = () => {
                 </template>
                 <template v-else>
                 <li>
-                    <a
-                        href="#"
+                    <router-link to="/login"
                         class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
                     >
                         <svg
@@ -577,16 +576,15 @@ const logout = () => {
                                 d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"
                             />
                         </svg>
-                        <router-link to="/login">
+                        <div>
                             <span class="flex-1 ms-3 whitespace-nowrap"
                                 >Sign In</span
                             >
-                        </router-link>
-                    </a>
+                        </div>
+                    </router-link>
                 </li>
                 <li>
-                    <a
-                        href="#"
+                    <router-link to="/register"
                         class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
                     >
                         <svg
@@ -606,12 +604,12 @@ const logout = () => {
                                 d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z"
                             />
                         </svg>
-                        <router-link to="/register">
+                        <div>
                             <span class="flex-1 ms-3 whitespace-nowrap"
                                 >Sign Up</span
                             >
-                        </router-link>
-                    </a>
+                        </div>
+                    </router-link>
                 </li>
             </template>
             </ul>
