6b8aebae193a773acbc269f9e6f85596ee4e78f4
RevaPrasetya <revabro3@gmail.com>
2024-01-04 12:47:18 +0800

    Automate

--- frontend-vue/src/components/Penjualan/TambahPenjualan.vue
+++ frontend-vue/src/components/Penjualan/TambahPenjualan.vue
@@ -1,5 +1,5 @@
 <script>
-import axios from "axios";
+    import axios from 'axios';
 
     export default {
         data(){
@@ -14,11 +14,23 @@ import axios from "axios";
             }
         },
         mounted(){
+            this.tambahPenjualan();
         },
         methods: {
             tambahPenjualan(){
+                const date = new Date();
+
+                let day = date.getDate();
+                let month = date.getMonth() + 1;
+                let year = date.getFullYear();
+                let datenow = Date.now();
+
+                let currentDateCode = `${day}-${month}-${year}-${datenow}`;
+
+                this.dataPenjualan.nomortransaksi = currentDateCode;
+                
                 axios
-                .post(`https://api-group9-prognet.manpits.xyz/api/penjualan`, this.dataPenjualan)
+                .post("https://api-group9-prognet.manpits.xyz/api/penjualan", this.dataPenjualan)
                 .then((response) => {
                     const newPenjualanId = response.data.id;
                     this.$router.push({ path: `/penjualan/detail/${newPenjualanId}`
@@ -29,32 +41,21 @@ import axios from "axios";
 </script>
 
 <template>
+    
     <div class="relative pt-12">
         <div class="py-5">
+            
             <nav class="flex" aria-label="Breadcrumb">
-                <ol
-                    class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse"
-                >
-                    <li class="inline-flex items-center">
-                        <router-link
-                            to="/"
-                            class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
-                        >
-                            <svg
-                                class="w-3 h-3 me-2.5"
-                                aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                            >
-                                <path
-                                    d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z"
-                                />
-                            </svg>
-                            Penjualan
-                        </router-link>
-                    </li>
-                    <!-- <li>
+            <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+                <li class="inline-flex items-center">
+                    <router-link to="/" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                        <svg class="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z"/>
+                        </svg>
+                        Penjualan
+                    </router-link>
+                </li>
+                <!-- <li>
                 <div class="flex items-center">
                     <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
@@ -62,75 +63,37 @@ import axios from "axios";
                     <a href="#" class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">Daftar Penjualan</a>
                 </div>
                 </li> -->
-                    <li aria-current="page">
-                        <div class="flex items-center">
-                            <svg
-                                class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
-                                aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 6 10"
-                            >
-                                <path
-                                    stroke="currentColor"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="m1 9 4-4-4-4"
-                                />
-                            </svg>
-                            <span
-                                class="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400"
-                                >Edit Penjualan</span
-                            >
-                        </div>
-                    </li>
-                </ol>
+                <li aria-current="page">
+                    <div class="flex items-center">
+                        <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                        </svg>
+                        <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">Edit Penjualan</span>
+                    </div>
+                </li>
+            </ol>
             </nav>
-        </div>
 
-        <div
-            class="max-w-lg mx-auto py-2 px-8 border border-gray-200 rounded-xl"
-        >
+        </div>
+        
+        <div class="max-w-lg mx-auto py-2 px-8 border border-gray-200 rounded-xl">
             <div class="py-4 mb-5">
-                <h3 class="text-2xl font-semibold text-blue-500">
-                    Tambah Transaksi
-                </h3>
+                <h3 class="text-2xl font-semibold text-blue-500">Tambah Transaksi</h3>
             </div>
 
-            <form @submit.prevent="tambahPenjualan">
+            <form @submit.prevent="tambahPenjualan" >
                 <div class="relative z-0 w-full mb-5 group">
-                    <input
-                        type="text"
-                        v-model="dataPenjualan.nomortransaksi"
-                        id="nomor_transaksi"
-                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                        placeholder=" "
-                        required
-                    />
-                    <label
-                        for="nomor_transaksi"
-                        class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                        >Nomor Transaksi</label
-                    >
+                    <input type="text" v-model="dataPenjualan.nomortransaksi" id="nomor_transaksi" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <label for="nomor_transaksi" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Nomor Transaksi</label>
                 </div>
 
-                <input
-                    type="hidden"
-                    v-model="dataPenjualan.user_id"
-                    id="nomor_transaksi"
-                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                    placeholder=" "
-                    required
-                />
+                <input type="hidden" v-model="dataPenjualan.user_id" id="nomor_transaksi" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
 
-                <button
-                    type="submit"
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                    Submit
-                </button>
+
+                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
             </form>
         </div>
-    </div>
+
+    </div>        
+    
 </template>
