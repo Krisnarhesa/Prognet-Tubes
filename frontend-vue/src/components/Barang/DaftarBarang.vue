d72d3b320ef2dfacae0a67ed8ed8843a0f6a5d3a
Krisna Rhesa <pradnyana.2205551073@student.unud.ac.id>
2024-01-09 17:25:21 +0800

    adjust UI dikit

--- frontend-vue/src/components/Barang/DaftarBarang.vue
+++ frontend-vue/src/components/Barang/DaftarBarang.vue
@@ -133,7 +133,7 @@ export default {
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                     </svg>
                 </div>
-                <input type="text" id="table-search-users" class="block pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Cari barang" v-model="searchValues.barang" @input="filterBarang">
+                <input type="text" id="table-search-users" class="block pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Cari Nama/Kode Barang" v-model="searchValues.barang" @input="filterBarang">
             </div>
         </div>
 
